fde8f5eafcde8bd5d651c3522db39907ca3885e1
j3rryl <jeremymunroe0@gmail.com>
2022-12-17 07:31:10 +0300

    ch..11

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 function BlogPage() {
+  const router = useRouter()
+  const handleMe=()=>{
+    console.log('Order placing')
+    router.push('/product')
+  }
   return (
     <div>
         <h1>BlogPage</h1>
         <Link href='/product'>
             Product
         </Link>
+        <button onClick={handleMe}>Place Me</button>
     </div>
   )
 }
