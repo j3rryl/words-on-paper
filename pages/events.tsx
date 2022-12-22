3269f953920b98bc04965b153cbec7c14fb51e78
j3rryl <jeremymunroe0@gmail.com>
2022-12-22 20:52:34 +0300

    basics done

---
+++ pages/events.tsx
@@ -0,0 +1,43 @@
+import { InferGetServerSidePropsType } from "next"
+import { useRouter } from "next/router"
+import { useState } from "react"
+
+function EventList({news}: InferGetServerSidePropsType<typeof getServerSideProps>){
+    const [events, setEvents] = useState(news)
+    const router = useRouter()
+    const filterThis= async () =>{
+        const response=await fetch(`http://localhost:5000/news?category=sports`)
+    const data = await response.json()
+    setEvents(data)
+    router.push('events?category=politics', undefined, {shallow:true})
+    }
+    return (
+        <>
+        <button onClick={filterThis}>Sports</button>
+        {events.map((newss: any)=>{
+            return(
+            <div key={newss.id}>
+                {newss.title}
+                <br />
+                {newss.author}
+                <hr />
+            </div>
+            )
+        })}
+        </>
+    )
+
+}
+export default EventList
+export async function getServerSideProps(context: any){
+    const {query} = context
+    const {category} = query
+    const queryString = category ? 'category=sports': ''
+    const response=await fetch(`http://localhost:5000/news?${queryString}`)
+    const data = await response.json()
+    return {
+        props: {
+            news: data,
+        }
+    }
+}
